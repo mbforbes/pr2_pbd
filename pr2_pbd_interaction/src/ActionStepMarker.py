a04dbac7926bee1a5b17cbff95c61512e4b3c110
Maxwell Forbes <m.b.forbes@gmail.com>
2014-02-22 21:48:32 -0800

    Make object reference arrows more green (like objects); TBD whether this is helpful, as object scanning not working in simulation.

--- pr2_pbd_interaction/src/ActionStepMarker.py
+++ pr2_pbd_interaction/src/ActionStepMarker.py
@@ -359,7 +359,7 @@ class ActionStepMarker:
                         lifetime=rospy.Duration(2),
                         scale=Vector3(0.02, 0.03, 0.04),
                         header=Header(frame_id=frame_id),
-                        color=ColorRGBA(1.0, 0.8, 0.2, 0.5),
+                        color=ColorRGBA(2.0, 0.8, 0.0, 0.6),
                         points=[pose.position, Point(0, 0, 0)]))
 
         # Calculate text position so that they "orbit" around the marker;
