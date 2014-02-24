b386ec35304457935096eb6e8a1b615cd0461d18
Maxwell Forbes <m.b.forbes@gmail.com>
2014-02-23 20:38:04 -0800

    Very close; changing vector printing to brackets for easy numpy loading.

--- pr2_pbd_interaction/src/World.py
+++ pr2_pbd_interaction/src/World.py
@@ -532,8 +532,8 @@ class World:
          '''
         # TODO(max): Actually write this.
         w = '' if not hasattr(vector_like, 'w') else ', ' + str(vector_like.w)
-        return '(' + str(vector_like.x) + ', ' + str(vector_like.y) + ', ' + \
-            str(vector_like.z) + w + ')'
+        return '[' + str(vector_like.x) + ', ' + str(vector_like.y) + ', ' + \
+            str(vector_like.z) + w + ']'
 
     def _publish_tf_pose(self, pose, name, parent):
         ''' Publishes a TF for object pose'''
