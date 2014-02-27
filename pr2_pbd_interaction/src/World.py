8fa67eef8904900c9ab31736a915db36881da529
Maxwell Forbes <m.b.forbes@gmail.com>
2014-02-26 22:36:46 -0800

    remove todo.

--- pr2_pbd_interaction/src/World.py
+++ pr2_pbd_interaction/src/World.py
@@ -539,7 +539,6 @@ class World:
          - Vector3 (e.g. boudning box dimensions)
          - etc.
          '''
-        # TODO(max): Actually write this.
         w = '' if not hasattr(vector_like, 'w') else ', ' + str(vector_like.w)
         return '[' + str(vector_like.x) + ', ' + str(vector_like.y) + ', ' + \
             str(vector_like.z) + w + ']'
