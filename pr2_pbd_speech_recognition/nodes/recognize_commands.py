693822161d564c433fa679c6de8c4f2a05cce047
Maxwell Forbes <m.b.forbes@gmail.com>
2014-05-09 18:04:44 -0700

    Add speech recognition with google API, add call / receive to NLP semantic parsing folks, end-to-end running OMG.

--- pr2_pbd_speech_recognition/nodes/recognize_commands.py
+++ pr2_pbd_speech_recognition/nodes/recognize_commands.py
@@ -1,48 +1,100 @@
 #!/usr/bin/env python
 
+# ROS imports come first
 import roslib
 roslib.load_manifest('pr2_pbd_speech_recognition')
 import rospy
+
+# 3rd party
+import requests
+
+# Local imports
 from std_msgs.msg import String
 from pr2_pbd_speech_recognition.msg import Command
+from pr2_pbd_speech_recognition.msg import RecognizedSpeech
+
+# Constants
+NLP_SERVER = 'http://localhost'
+NLP_PORT = 10001
 
 class CommandRecognizer:
-    def __init__(self):
-        rospy.Subscriber('recognizer/output', String, self.receiveSphinxData)
-        self.commandOutput = rospy.Publisher('recognized_command', Command)
-        self.allCommands = [Command.TEST_MICROPHONE, 
-		Command.RELAX_RIGHT_ARM, 
-		Command.RELAX_LEFT_ARM, 
-		Command.OPEN_RIGHT_HAND, 
-		Command.OPEN_LEFT_HAND, 
-		Command.CLOSE_RIGHT_HAND, 
-		Command.CLOSE_LEFT_HAND, 
-		Command.STOP_EXECUTION, 
-		Command.UNDO, 
-		Command.DELETE_ALL_STEPS, 
-		Command.DELETE_LAST_STEP, 
-		Command.FREEZE_RIGHT_ARM, 
-		Command.FREEZE_LEFT_ARM, 
-		Command.RECORD_OBJECT_POSE, 
-		Command.CREATE_NEW_ACTION, 
-		Command.EXECUTE_ACTION, 
-		Command.NEXT_ACTION, 
-		Command.PREV_ACTION, 
-		Command.SAVE_ACTION, 
-		Command.EDIT_ACTION, 
-		Command.SAVE_POSE, 
-		Command.START_RECORDING_MOTION, 
-		Command.STOP_RECORDING_MOTION]
+    '''This class is responsible for receiving recognized speech,
+    transforming it into a system command, and publishing that
+    command.'''
 
-    def receiveSphinxData(self,data):
+    def __init__(self):
+        # Subscribe to pocketsphinx (if it exists).
+        rospy.Subscriber('recognizer/output', String, self.receiveSphinxData)
+
+        # Subscribe to gspeech (if it exists).
+        rospy.Subscriber('gspeech/output', RecognizedSpeech,
+            self.receiveGspeechData)
+
+        # Here's what we'll give back to the system.
+        self.commandOutput = rospy.Publisher('recognized_command', Command)
+        self.allCommands = [
+            Command.TEST_MICROPHONE,
+            Command.RELAX_RIGHT_ARM,
+            Command.RELAX_LEFT_ARM,
+            Command.OPEN_RIGHT_HAND,
+            Command.OPEN_LEFT_HAND,
+            Command.CLOSE_RIGHT_HAND,
+            Command.CLOSE_LEFT_HAND,
+            Command.STOP_EXECUTION,
+            Command.UNDO,
+            Command.DELETE_ALL_STEPS,
+            Command.DELETE_LAST_STEP,
+            Command.FREEZE_RIGHT_ARM,
+            Command.FREEZE_LEFT_ARM,
+            Command.RECORD_OBJECT_POSE,
+            Command.CREATE_NEW_ACTION,
+            Command.EXECUTE_ACTION,
+            Command.NEXT_ACTION,
+            Command.PREV_ACTION,
+            Command.SAVE_ACTION,
+            Command.EDIT_ACTION,
+            Command.SAVE_POSE,
+            Command.START_RECORDING_MOTION,
+            Command.STOP_RECORDING_MOTION
+        ]
+
+    def receiveGspeechData(self, recognizedSpeech):
+        '''Gspeech should recognize arbitrary speech. We'll then send
+        this to the NLP semantic-parsing pipeline.'''
+        params = {'sentence': recognizedSpeech.text}
+        print 'RECOGNIZER RETURNED:', recognizedSpeech.text
+        try:
+            response = requests.get('%s:%d' % (NLP_SERVER, NLP_PORT),
+                params=params).text.strip()
+            # Make sure we got something non-empty back.
+            print 'NLP RETURNED:', response
+            if len(response) > 0 and len(eval(response)) > 0:
+                cmd = eval(response)[0]
+                # Do sanity checking.
+                if cmd in self.allCommands:
+                    recognizedCommand = cmd
+                else:
+                    recognizedCommand = Command.UNRECOGNIZED
+                # Send it off
+                self.commandOutput.publish(Command(recognizedCommand))
+        except requests.exceptions.ConnectionError as e:
+            rospy.logwarn('NLP server not running. Dropping recognized ' +
+                'speech: ' + recognizedSpeech.text)
+
+    def receiveSphinxData(self, data):
+        '''Sphinx is trained only with the commands.'''
+        print data
         recognizedStr = data.data
         recognizedCommand = Command.UNRECOGNIZED
-        
+
+        # NOTE(max): Testing full pocketsphinx capabilities.
+        rospy.loginfo('Sphinx got: ' + recognizedStr)
+
         for commandStr in self.allCommands:
             if (recognizedStr == commandStr):
                 recognizedCommand = commandStr
-                
-        rospy.loginfo('Received command:' + recognizedCommand)
+
+        #rospy.loginfo('Received command:' + recognizedCommand)
         command = Command()
         command.command = recognizedCommand
         self.commandOutput.publish(command)
---
+++ pr2_pbd_speech_recognition/nodes/recognize_speech.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+
+# ros package for speech recognition using Google Speech API
+#
+# written by achuwilson
+# 30-06-2012 , 3.00pm
+# achu@achuwilson.in
+#
+# modified by Zhongyue (Jerry) Zhang
+# 3/19/2014
+# jerryzh.cn@gmail.com
+#
+# modified by Andrzej Pronobis
+# 4/01/2014
+# a.pronobis@gmail.com
+#
+# modified by Maxwell Forbes
+# 5/9/2014
+# m.b.forbes@gmail.com
+
+# ROS imports first
+import roslib
+roslib.load_manifest('pr2_pbd_speech_recognition')
+import rospy
+
+# System imports
+import json
+import os
+import shlex
+import subprocess
+import sys
+
+# Local imports
+from pr2_pbd_speech_recognition.msg import RecognizedSpeech
+
+# Constants
+# ----------------------------------------------------------------------
+
+# URL / comamnd constants
+KEY = 'AIzaSyCnl6MRydhw_5fLXIdASxkLJzcJh5iX0M4'
+API_URL = 'http://www.google.com/speech-api/v2/recognize?lang=en-us&key='
+FILENAME_BASE = 'recording.flac'
+FILENAME_NORMED = 'recordingNorm.flac'
+HEADER = 'Content-Type: audio/x-flac; rate=16000" -O - '
+
+# Audio constants.
+# SILENCE_END: after we start hearing sound, how long of a pause we need
+# before we consider the utterance 'finished' and send it off to the
+# speech recognizer.
+SILENCE_END = '0.05' # was 2.0 originally
+
+# DEFAULT_CONFIDENCE: Sometimes Google's API won't return a confidence
+# for its top guess. This is the default that we use.
+DEFAULT_CONFIDENCE = 0.75
+
+# The string and confidence we use when we got an error or nothing
+# recognized back from the speech recognizer.
+BAD_STR, BAD_CONF = '', 0.0
+
+# The commands themselves.
+# Record speech.
+cmd_record = ('sox -q -r 16000 -t alsa default ' +
+    FILENAME_BASE +
+    ' silence 1 0.1 0.8% ' + SILENCE_END + ' 1.5 1%')
+#   ' silence 1 0.1 0.8% 2 1.5 1%') # orig
+
+# Send the data to Google speech recognition service.
+cmd_recognize = ('wget -q -U "Mozilla/5.0" --post-file ' +
+    FILENAME_NORMED +
+    ' --header="' + HEADER + '"' +
+    API_URL + KEY + '"')
+
+# Normalize the audio (it seems to improve recognition)
+cmd_norm='sox ' + FILENAME_BASE + ' ' + FILENAME_NORMED + ' gain -n -3'
+
+# Functions
+# ----------------------------------------------------------------------
+
+def speech():
+    '''Runs in a loop to recognize speech and publish the result.'''
+    rospy.init_node('gspeech')
+    pub = rospy.Publisher('gspeech/output', RecognizedSpeech)
+
+    args_recognize = shlex.split(cmd_recognize)
+    while not rospy.is_shutdown():
+        os.system(cmd_record)
+        os.system(cmd_norm)
+        output, error = subprocess.Popen(
+            args_recognize,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE).communicate()
+
+        # my own processing
+        if not error:
+            if len(output.splitlines()) < 2:
+                # Didn't get a good result.
+                trans, conf = BAD_STR, BAD_CONF
+            else:
+                js = json.loads(output.splitlines()[1])
+                idx = js['result_index']
+                top = js['result'][0]['alternative'][idx]
+                if top.has_key('confidence'):
+                    conf = top['confidence']
+                else:
+                    conf = DEFAULT_CONFIDENCE
+                trans = str(top['transcript'])
+            # Publish.
+            pub.publish(RecognizedSpeech(trans, conf))
+
+if __name__ == '__main__':
+    rospy.init_node('gspeech')
+    try:
+        speech()
+    except rospy.ROSInterruptException: pass
+    except KeyboardInterrupt:
+        sys.exit(1)
